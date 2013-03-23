287d6aaa68d7bd550fbe7a888999deb066101aa0
elag <elag@kologlobal.com>
2013-03-23 14:09:41 +0200

    Initial Commit

---
+++ songkickmap/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  songkickmap
+//
+//  Created by Oleg Agapov on 3/23/13.
+//  Copyright (c) 2013 Oleg Agapov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ songkickmap/SKViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKViewController.h
+//  songkickmap
+//
+//  Created by Oleg Agapov on 3/23/13.
+//  Copyright (c) 2013 Oleg Agapov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKViewController : UIViewController
+
+@end
